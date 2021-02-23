7a4ac528df8c75fbcc5df602a7a3d59e4bff62d9
Jade McDaniels <jademcdaniels81@gmail.com>
2021-02-22 23:28:32 -0500

    commit tree 12a0ffd004142dbdfb03a2c1486ae3faddbb5f1f

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+
+let mapToNegativize= 
+    function(sourceArray){
+   return sourceArray.map(x => x * -1);
+}
+
+ function mapToNoChange(sourceArray){
+    return sourceArray.map(x=>x);
+ }
+    
+ function mapToDouble(sourceArray){
+     return sourceArray.map(x => x * 2);
+ }
+
+ function mapToSquare(sourceArray){
+     return sourceArray.map(x => x * x);
+ }
+
+
+
+
+ function reduceToTotal(sourceArray, startingPoint){
+     const reducer = (accumulator, startingPoint) => accumulator + startingPoint;
+        return sourceArray.reduce(reducer);
+ }
+
+
+ function reduceToAllTrue(sourceArray){
+     const reducer = (sourceArray) => sourceArray == 1 
+     return sourceArray.reduce(reducer);
+ }
